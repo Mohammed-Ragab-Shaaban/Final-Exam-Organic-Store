f4bdd4b6caca0435a992dce0816cb6c8ce58027e
Mohammed-Ragab-Shaaban <mohammedragabshaaban2@gmail.com>
2024-09-04 18:28:20 +0300

    third commit

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Section1 from "./Section1";
+import './HomePage.css'
 
 export default function HomePage() {
   return (
-    <div>HomePage
-        <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
+    <div>
+        <Section1 />
     </div>
   )
 }
