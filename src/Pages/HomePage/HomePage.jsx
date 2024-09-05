789ca5e6dac821b454d23149a6e9db10bd97e7e8
Mohammed-Ragab-Shaaban <mohammedragabshaaban2@gmail.com>
2024-09-05 14:17:25 +0300

    forth commit

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -2,11 +2,13 @@ import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Section1 from "./Section1";
 import './HomePage.css'
+import Section2 from "./Section2";
 
 export default function HomePage() {
   return (
     <div>
         <Section1 />
+        <Section2 />
     </div>
   )
 }
